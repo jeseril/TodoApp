1fdad1dfe0437c119223c4f2df387aaca38587c3
Jesus <jeseril2213@gmail.com>
2023-10-23 11:40:33 -0500

    Prueba push

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -3,7 +3,7 @@ import './TodoCounter.css'
 function TodoCounter({text,total,completed}) {
     return(
         <>
-        <h1 className='YourTask'>Your Task</h1>
+        <h1 className='YourTask'>Your Task - Prueba</h1>
         <h1>{text} {completed} de {total} TODOS</h1>
         </>
       
